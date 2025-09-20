69e4a933d279fc89691a4c781ff0a71295b1fc8d
Malika <zhalilovamalika98@gmail.com>
2025-09-20 15:55:19 +0500

    Задание по обьектом - готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,48 @@
-﻿
+﻿function parseCount(value) {
+    const result = Number.parseFloat(value);
+    if (Number.isNaN(result)) {
+      throw new Error('Невалидное значение');
+    }
+    return result;
+  }
+  
+  function validateCount(value) {
+    try {
+      return parseCount(value);
+    } catch (e) {
+      return e;
+    }
+  }
+  
+  class Triangle {
+    constructor(a, b, c) {
+      const invalid = (a + b) <= c || (a + c) <= b || (b + c) <= a;
+      if (invalid) {
+        throw new Error('Треугольник с такими сторонами не существует');
+      }
+      this.a = a;
+      this.b = b;
+      this.c = c;
+    }
+  
+    get perimeter() {
+      return this.a + this.b + this.c;
+    }
+  
+    get area() {
+      const p = this.perimeter / 2;
+      const s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+      return Number(s.toFixed(3));
+    }
+  }
+  
+  function getTriangle(a, b, c) {
+    try {
+      return new Triangle(a, b, c);
+    } catch (e) {
+      return Object.freeze({
+        get perimeter() { return 'Ошибка! Треугольник не существует'; },
+        get area() { return 'Ошибка! Треугольник не существует'; }
+      });
+    }
+  }
